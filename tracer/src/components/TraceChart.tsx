6cc25bba33c97d25ee8ab6ee7c313090de8c0097
ya-vera <143505326+ya-vera@users.noreply.github.com>
2025-07-13 22:59:12 +0300

    changed TraceChart

--- tracer/src/components/TraceChart.tsx
+++ tracer/src/components/TraceChart.tsx
@@ -1,23 +1,31 @@
-import { Component, onMount } from 'solid-js';
+import { Component, createEffect } from 'solid-js';
 import * as d3 from 'd3';
 import { state } from '../store/traceStore';
 
 const TraceChart: Component = () => {
     let ref!: SVGSVGElement;
 
-    onMount(() => {
+    createEffect(() => {
         const svg = d3.select(ref);
         svg.selectAll('*').remove();
 
         if (!state.selected.length) {
             svg.append('text')
-               .attr('x', 20)
-               .attr('y', 20)
-               .text('Select parameter');
+                .attr('x', 20)
+                .attr('y', 20)
+                .text('Select parameter');
             return;
         }
 
-        const data = state.data[state.selected[0]].slice(state.burnin);
+        const data = state.data[state.selected[0]]?.slice(state.burnin) ?? [];
+        if (data.length === 0) {
+            svg.append('text')
+                .attr('x', 20)
+                .attr('y', 20)
+                .text('No data available');
+            return;
+        }
+
         const width = 600;
         const height = 300;
         const margin = { top: 20, right: 20, bottom: 40, left: 50 };
@@ -36,22 +44,22 @@ const TraceChart: Component = () => {
             .y(v => y(v));
 
         svg.append('g')
-           .attr('transform', `translate(0,${height - margin.bottom})`)
-           .call(d3.axisBottom(x));
+            .attr('transform', `translate(0,${height - margin.bottom})`)
+            .call(d3.axisBottom(x));
 
         svg.append('g')
-           .attr('transform', `translate(${margin.left},0)`)
-           .call(d3.axisLeft(y));
+            .attr('transform', `translate(${margin.left},0)`)
+            .call(d3.axisLeft(y));
 
         svg.append('path')
-           .datum(data)
-           .attr('fill', 'none')
-           .attr('stroke', 'steelblue')
-           .attr('stroke-width', 1.5)
-           .attr('d', line as any);
+            .datum(data)
+            .attr('fill', 'none')
+            .attr('stroke', 'steelblue')
+            .attr('stroke-width', 1.5)
+            .attr('d', line as any);
     });
 
-    return <svg ref={ref} width="600" height="300"></svg>;
+    return <svg ref={ref} width="600" height="300" />;
 };
 
 export default TraceChart;
