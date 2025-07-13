f688e79e6fca33c407f79323e8740cf368e65725
Alina Tikhomirova <atshka8@gmail.com>
2025-07-13 20:54:24 +0300

    Шкала у Trace

--- tracer/src/components/TraceChart.tsx
+++ tracer/src/components/TraceChart.tsx
@@ -4,36 +4,54 @@ import { state } from '../store/traceStore';
 
 const TraceChart: Component = () => {
     let ref!: SVGSVGElement;
-    const { selected, data, burnin } = state;
+
     onMount(() => {
         const svg = d3.select(ref);
         svg.selectAll('*').remove();
-        if (!selected.length) {
-            svg.append('text').attr('x', 20).attr('y', 20).text('Select parameter');
+
+        if (!state.selected.length) {
+            svg.append('text')
+               .attr('x', 20)
+               .attr('y', 20)
+               .text('Select parameter');
             return;
         }
-        const arr = data[selected[0]].slice(burnin);
-        const width = 600,
-            height = 300;
-        svg.attr('viewBox', `0 0 ${width} ${height}`);
-        const x = d3
-            .scaleLinear()
-            .domain([0, arr.length - 1])
-            .range([0, width]);
-        const y = d3
-            .scaleLinear()
-            .domain([d3.min(arr)!, d3.max(arr)!])
-            .range([height, 0]);
-        const line = d3
-            .line<number>()
+
+        const data = state.data[state.selected[0]].slice(state.burnin);
+        const width = 600;
+        const height = 300;
+        const margin = { top: 20, right: 20, bottom: 40, left: 50 };
+
+        const x = d3.scaleLinear()
+            .domain([0, data.length - 1])
+            .range([margin.left, width - margin.right]);
+
+        const y = d3.scaleLinear()
+            .domain([d3.min(data)!, d3.max(data)!])
+            .nice()
+            .range([height - margin.bottom, margin.top]);
+
+        const line = d3.line<number>()
             .x((_, i) => x(i))
-            .y((v) => y(v));
+            .y(v => y(v));
+
+        svg.append('g')
+           .attr('transform', `translate(0,${height - margin.bottom})`)
+           .call(d3.axisBottom(x));
+
+        svg.append('g')
+           .attr('transform', `translate(${margin.left},0)`)
+           .call(d3.axisLeft(y));
+
         svg.append('path')
-            .datum(arr)
-            .attr('fill', 'none')
-            .attr('stroke', 'steelblue')
-            .attr('d', line as any);
+           .datum(data)
+           .attr('fill', 'none')
+           .attr('stroke', 'steelblue')
+           .attr('stroke-width', 1.5)
+           .attr('d', line as any);
     });
+
     return <svg ref={ref} width="600" height="300"></svg>;
 };
+
 export default TraceChart;
