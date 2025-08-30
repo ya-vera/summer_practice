c17fe38e9bf8bdf270624e8cbedd369528a654b5
ya-vera <143505326+ya-vera@users.noreply.github.com>
2025-08-30 13:14:25 +0300

    add zoom and tooltip

--- tracer/src/components/TraceChart.tsx
+++ tracer/src/components/TraceChart.tsx
@@ -30,6 +30,14 @@ const TraceChart: Component = () => {
         const height = 300;
         const margin = { top: 20, right: 20, bottom: 40, left: 50 };
 
+        svg.append('defs').append('clipPath')
+            .attr('id', 'clip')
+            .append('rect')
+            .attr('width', width - margin.left - margin.right)
+            .attr('height', height - margin.top - margin.bottom)
+            .attr('x', margin.left)
+            .attr('y', margin.top);
+
         const x = d3.scaleLinear()
             .domain([0, data.length - 1])
             .range([margin.left, width - margin.right]);
@@ -43,20 +51,62 @@ const TraceChart: Component = () => {
             .x((_, i) => x(i))
             .y(v => y(v));
 
-        svg.append('g')
+        const xAxis = svg.append('g')
+            .attr('class', 'x-axis')
             .attr('transform', `translate(0,${height - margin.bottom})`)
             .call(d3.axisBottom(x));
 
-        svg.append('g')
+        const yAxis = svg.append('g')
+            .attr('class', 'y-axis')
             .attr('transform', `translate(${margin.left},0)`)
             .call(d3.axisLeft(y));
 
-        svg.append('path')
+        const lineGroup = svg.append('g')
+            .attr('clip-path', 'url(#clip)');
+
+        lineGroup.append('path')
             .datum(data)
             .attr('fill', 'none')
             .attr('stroke', 'steelblue')
             .attr('stroke-width', 1.5)
             .attr('d', line as any);
+
+        const zoom = d3.zoom<SVGSVGElement, unknown>()
+            .scaleExtent([0.5, 10])
+            .extent([[margin.left, 0], [width - margin.right, height]])
+            .on('zoom', (event: d3.D3ZoomEvent<SVGSVGElement, unknown>) => {
+                const transform = event.transform;
+                const newX = transform.rescaleX(x);
+                lineGroup.select('path').attr('d', line.x((_, i) => newX(i)) as any);
+                xAxis.call(d3.axisBottom(newX));
+            });
+        svg.call(zoom);
+
+        const tooltip = svg.append('g')
+            .attr('class', 'tooltip')
+            .style('display', 'none');
+
+        tooltip.append('circle')
+            .attr('r', 4)
+            .attr('fill', 'red');
+
+        tooltip.append('text')
+            .attr('x', 8)
+            .attr('dy', '.35em')
+            .attr('fill', 'black');
+
+        svg.on('mouseover', () => tooltip.style('display', null))
+           .on('mouseout', () => tooltip.style('display', 'none'))
+           .on('mousemove', (event) => {
+               const [xMouse] = d3.pointer(event);
+               const xValue = x.invert(xMouse);
+               const bisectIndex = d3.bisectLeft(data.map((_, i) => i), xValue);
+               const index = Math.min(Math.max(bisectIndex - 1, 0), data.length - 1);
+               const value = data[index];
+
+               tooltip.attr('transform', `translate(${x(index)},${y(value)})`);
+               tooltip.select('text').text(`${state.selected[0]}: ${value.toFixed(3)}`);
+           });
     });
 
     return <svg ref={ref} width="600" height="300" />;
